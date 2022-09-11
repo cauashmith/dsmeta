0aa5869e06d0f27694255434e9a5cf89cd919a3f
Caua <contatocaua.shmith@gmail.com>
2022-09-10 21:05:22 -0300

    Range de datas para busca de vendas

--- backend/src/main/java/com/example/dsmeta/repository/SaleRepository.java
+++ backend/src/main/java/com/example/dsmeta/repository/SaleRepository.java
@@ -1,10 +1,20 @@
 package com.example.dsmeta.repository;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.dsmeta.entities.Sale;
 
 // componente do sistema responsavel por acessar o banco de dados
 public interface SaleRepository extends JpaRepository<Sale, Long>{
+	
+	@Query(value=" SELECT obj FROM Sale obj " + 
+				 " WHERE obj.date BETWEEN :min AND :max " + 
+				 " ORDER BY obj.amount DESC ")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
 }
--- backend/src/main/java/com/example/dsmeta/service/SaleService.java
+++ backend/src/main/java/com/example/dsmeta/service/SaleService.java
@@ -1,8 +1,13 @@
 package com.example.dsmeta.service;
 
-import java.util.List;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.dsmeta.entities.Sale;
@@ -14,8 +19,20 @@ public class SaleService {
 	@Autowired
 	private SaleRepository saleRepository;
 	
-	public List<Sale> findSales() {
-		return saleRepository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);								
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		// no codigo acima, é feito um if ternário, onde se o min date for nulo, ou seja, se 
+		// não foi enviado um min date, é enviado um today, que é o instante atual menos 365 dias
+		// puxando os dados mínimos de um ano atrás, e como dados máximos, o dia de hoje.
+		
+		// converte o minDate (string) para uma data na forma de LocalDate
+		
+		return saleRepository.findSales(min, max, pageable);
 	}
 	
 }
